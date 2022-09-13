f3cead608f0a209ee31d2133e8eb98a292bc10c3
jkresak101 <jkresak101@gmail.com>
2022-09-13 08:57:50 -0700

    Added notes.

--- Fall2022-Classes/Fall2022-Classes/Program.cs
+++ Fall2022-Classes/Fall2022-Classes/Program.cs
@@ -12,3 +12,11 @@ var gavCarMake = "Toyota";
 
 Car gavinsCar = new Car(gavCarMake, "Camry", "Grey", 2007);
 Console.WriteLine("Gavin has a " + gavinsCar.Make);
+
+//making a list of objects from our new class
+var garage = new List<Car>();
+
+garage.Add(gavinsCar);
+garage.Add(ashleysCar);
+
+Console.WriteLine(garage[0].Model);
