441e88fe2f4c4f346d1941040691c163f21cab63
jkresak101 <jkresak101@gmail.com>
2022-09-13 08:51:14 -0700

    updated notes from class

---
+++ Fall2022-Classes/Fall2022-Classes/Car.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fall2022_Classes
+{
+    public class Car
+    {
+        //properties
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public int Year { get; set; }
+        public int Speed { get; set; }
+        public bool IsPaidFor { get; set; }
+        
+        //access ,pdifiers: public vs private vs interal
+        //how we can access classes, properties, methods,etc, in our applications 
+
+        //constructor
+        public Car()
+        {
+            Make = "Lexus";
+            Model = "RX 350";
+            Color = "White";
+            Year = 2018;
+            Speed = 0;
+            IsPaidFor = false;
+        }
+
+        public Car(string make, string model, string color, int year)
+        {
+            Make = make;
+            Model = model;
+            Color = color;
+            Year = year;
+        }
+
+        public Car(string make, string model, string color, int year, int speed, bool IsPaidFor)
+        {
+            Make = make;
+            Model = model;
+            Color = color;
+            Year = year;
+        }
+
+        //methods
+
+    } 
+}
---
+++ Fall2022-Classes/Fall2022-Classes/Program.cs
@@ -0,0 +1,14 @@
+﻿using Fall2022_Classes;
+
+Car ashleysCar = new Car();
+
+Console.WriteLine("Ashley has a "+ ashleysCar.Make);
+
+ashleysCar.IsPaidFor = true;
+
+Console.WriteLine("This car is paid for? : " + ashleysCar.IsPaidFor);
+
+var gavCarMake = "Toyota";
+
+Car gavinsCar = new Car(gavCarMake, "Camry", "Grey", 2007);
+Console.WriteLine("Gavin has a " + gavinsCar.Make);
